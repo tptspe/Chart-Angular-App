9bfea417602f49e6b6612e4488bf96fb68dece95
tri <tranphuoctri96@gmail.com>
2022-10-28 10:14:43 +0700

    change label

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -25,13 +25,13 @@ export class LineChartComponent implements OnInit {
 								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
 	       datasets: [
           {
-            label: "Sales",
+            label: "Old Product",
             data: ['467','576', '572', '79', '92',
 								 '574', '573', '576'],
             backgroundColor: 'blue'
           },
           {
-            label: "Profit",
+            label: "New Brand",
             data: ['542', '542', '536', '327', '17',
 									 '0.00', '538', '541'],
             backgroundColor: 'limegreen'
